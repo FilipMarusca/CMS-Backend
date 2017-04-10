56188b1a32a4bf8f82f3ed51158cbea469868053
Alexandra Muresan <alle.muresan@yahoo.com>
2017-04-10 19:38:53 +0300

    Module separation

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,10 +1,12 @@
+import com.ubb.cms.Conference;
+import com.ubb.cms.Edition;
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
 import org.hibernate.Transaction;
-import org.hibernate.boot.registry.StandardServiceRegistryBuilder;
 import org.hibernate.cfg.Configuration;
 import org.hibernate.exception.ConstraintViolationException;
 import org.hibernate.service.ServiceRegistry;
+import org.hibernate.service.ServiceRegistryBuilder;
 
 
 /**
@@ -15,7 +17,17 @@ public class Main {
     public static void main(String[] args){
 
 
-       
+        Edition e = new Edition();
+        Configuration con = new Configuration().configure("/hibernate.cfg.xml").addAnnotatedClass(Edition.class);
+        ServiceRegistry reg = new ServiceRegistryBuilder().applySettings(con.getProperties()).buildServiceRegistry();
+        SessionFactory sf = con.buildSessionFactory(reg);
+        Session session = sf.openSession();
+        Transaction tx = session.beginTransaction();
+        session.save(e);
+        tx.commit();
+        session.close();
+        System.out.println("here");
+
 
     }
 }
