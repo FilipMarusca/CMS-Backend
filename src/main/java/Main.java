4a7b50f0685aae0fa056ca7f41e2b0565511f9c5
Alexandra Muresan <alle.muresan@yahoo.com>
2017-04-10 18:56:43 +0300

    "Initial commit"

---
+++ src/main/java/Main.java
@@ -0,0 +1,21 @@
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.Transaction;
+import org.hibernate.boot.registry.StandardServiceRegistryBuilder;
+import org.hibernate.cfg.Configuration;
+import org.hibernate.exception.ConstraintViolationException;
+import org.hibernate.service.ServiceRegistry;
+
+
+/**
+ * Created by Alexandra Muresan on 4/5/2017.
+ */
+public class Main {
+
+    public static void main(String[] args){
+
+
+       
+
+    }
+}
