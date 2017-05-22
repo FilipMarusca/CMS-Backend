deed1ec89e199db8fb46a1a681d3e6d4adcc20c8
Raul <raul.muresan01@gmail.com>
2017-05-22 19:49:16 +0300

    Removed merge comment

--- repository/src/main/java/repository/EditionRepository.java
+++ repository/src/main/java/repository/EditionRepository.java
@@ -4,11 +4,6 @@ import com.ubb.cms.Edition;
 import org.hibernate.SessionFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
-<<<<<<< HEAD
-
-import java.util.List;
-=======
->>>>>>> c1d8492f42e82305b5b2e16c6ea69670ff4fe9e8
 
 /**
  * Created by Raul on 24/04/2017.
