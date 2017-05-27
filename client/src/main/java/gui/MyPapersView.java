ec4119c3568134b2054dfc6f0bf326e08e23fe46
Filip Marusca <marusca.filip@gmail.com>
2017-05-28 00:19:40 +0300

    loading papers in the ‘My papers’ screen

--- client/src/main/java/gui/MyPapersView.java
+++ client/src/main/java/gui/MyPapersView.java
@@ -1,6 +1,15 @@
 package gui;
 
+import com.ubb.cms.Edition;
+import com.ubb.cms.Paper;
+import com.ubb.cms.utils.PaperStatus;
+import com.ubb.cms.utils.PaperTopics;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+import javafx.scene.control.cell.PropertyValueFactory;
 
 import java.net.URL;
 import java.util.ResourceBundle;
@@ -10,14 +19,39 @@ import java.util.ResourceBundle;
  */
 public class MyPapersView extends BaseView {
 
+    @FXML
+    private TableView<Paper> table;
+    @FXML
+    private TableColumn<Paper, String> paperNameColumn;
+    @FXML
+    private TableColumn<Paper, PaperTopics> topicColumn;
+    @FXML
+    private TableColumn<Paper, Edition> editionColumn;
+    @FXML
+    private TableColumn<Paper, PaperStatus> statusColumn;
+    @FXML
+    private ObservableList<Paper> model;
+
     @Override
     public void update() {
-
+        try {
+            model.clear();
+            model.addAll(controller.getAllPapers());
+        } catch (Exception exception) {
+            ShowAlert.showAlert("Failed to load papers");
+            ShowAlert.showAlert(exception.getMessage());
+        }
     }
 
     @Override
     public void initialize(URL location, ResourceBundle resources) {
+        paperNameColumn.setCellValueFactory(new PropertyValueFactory<>("title"));
+        topicColumn.setCellValueFactory(new PropertyValueFactory<>("topic"));
+        editionColumn.setCellValueFactory(new PropertyValueFactory<>("edition_id"));
+        statusColumn.setCellValueFactory(new PropertyValueFactory<>("status"));
 
+        model = FXCollections.observableArrayList();
+        table.setItems(model);
     }
 
     @FXML
@@ -27,5 +61,10 @@ public class MyPapersView extends BaseView {
         }catch(Exception ex){
             ShowAlert.showAlert(ex.getMessage());
         }
+    }
+
+    @FXML
+    public void downloadHandler() {
+
     }
 }
