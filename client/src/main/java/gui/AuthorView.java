1aba1787561b95b9b40069db0029f8bc13683a29
Filip Marusca <marusca.filip@gmail.com>
2017-06-04 16:51:22 +0300

    added functionality to upload summary

--- client/src/main/java/gui/AuthorView.java
+++ client/src/main/java/gui/AuthorView.java
@@ -1,6 +1,5 @@
 package gui;
 
-import com.sun.org.apache.xalan.internal.xsltc.compiler.util.ErrorMessages_zh_CN;
 import com.ubb.cms.Edition;
 import com.ubb.cms.Paper;
 import com.ubb.cms.User;
@@ -12,7 +11,6 @@ import javafx.fxml.FXML;
 import javafx.scene.control.*;
 import javafx.scene.control.cell.PropertyValueFactory;
 import javafx.stage.FileChooser;
-import org.springframework.stereotype.Service;
 import service.exception.ServiceException;
 
 import java.io.*;
@@ -49,6 +47,8 @@ public class AuthorView extends BaseView {
 
     private byte[] pdfData;
 
+    private byte[] pdfDataSummary;
+
 
     @Override
     public void initialize(URL location, ResourceBundle resources) {
@@ -66,11 +66,42 @@ public class AuthorView extends BaseView {
     }
 
     @FXML
+    public void uploadSummaryHandler() {
+
+        //TODO fix the bug where the application crashes when no file is selected
+        FileChooser fileChooser = new FileChooser();
+        FileChooser.ExtensionFilter extFilter = new FileChooser.ExtensionFilter("PDF files (*.pdf)", "*.pdf");
+        fileChooser.getExtensionFilters().add(extFilter);
+        File file = fileChooser.showOpenDialog(currentStage);
+        logger.info(Long.toString(file.length()));
+
+        try {
+            final byte[] buffer = new byte[(int) (file.length())];
+            final InputStream in = new FileInputStream(file);
+            final ByteArrayOutputStream baos = new ByteArrayOutputStream();
+            logger.info(file.toString());
+
+            int read = -1;
+            while ((read = in.read(buffer)) > 0) {
+                baos.write(buffer, 0, read);
+            }
+            in.close();
+
+            pdfDataSummary = new byte[(int) file.length()];
+            DataInputStream dis = new DataInputStream(new FileInputStream(file));
+            dis.readFully(pdfDataSummary);  // read from file into byte[] array
+            dis.close();
+
+            // Paper paper = new Paper(author, edition, PaperStatus.InReview, title, topic, pdfData);
+            //controller.addPaper(paper);
+
+        } catch (Exception exception) {
+            logger.warning(exception.getMessage());
+        }
+    }
+
+    @FXML
     public void uploadHandler() {
-
-        //logger.info(topic);
-
-
 
         //TODO fix the bug where the application crashes when no file is selected
         FileChooser fileChooser = new FileChooser();
@@ -92,17 +123,12 @@ public class AuthorView extends BaseView {
             }
             in.close();
 
-            ByteArrayInputStream bais = new ByteArrayInputStream(baos.toByteArray());
-
             pdfData = new byte[(int) file.length()];
             DataInputStream dis = new DataInputStream(new FileInputStream(file));
             dis.readFully(pdfData);  // read from file into byte[] array
             dis.close();
 
-
-
-
-           // Paper paper = new Paper(author, edition, PaperStatus.InReview, title, topic, pdfData);
+            // Paper paper = new Paper(author, edition, PaperStatus.InReview, title, topic, pdfData);
             //controller.addPaper(paper);
 
         } catch (Exception exception) {
@@ -112,26 +138,25 @@ public class AuthorView extends BaseView {
 
 
     @FXML
-    void submitPaper()
-    {
-        if(table.getSelectionModel().getSelectedItem() == null)
-        {
+    void submitPaper() {
+        if (table.getSelectionModel().getSelectedItem() == null) {
             ShowAlert.showAlert("There is no selected Edition");
             return;
         }
-        if(pdfData == null || pdfData.length == 0)
-        {
+        if (pdfData == null || pdfData.length == 0) {
             ShowAlert.showAlert("No PDF uploaded");
             return;
         }
-        if(topicComboBox.getSelectionModel().getSelectedItem() == null)
-        {
+        if (pdfDataSummary == null || pdfDataSummary.length == 0) {
+            ShowAlert.showAlert("No PDF summary uploaded");
+            return;
+        }
+        if (topicComboBox.getSelectionModel().getSelectedItem() == null) {
             ShowAlert.showAlert("No topic selected");
             return;
         }
 
-        if(titleText.getText() == null)
-        {
+        if (titleText.getText() == null) {
             ShowAlert.showAlert("No title selected");
             return;
         }
@@ -141,30 +166,27 @@ public class AuthorView extends BaseView {
             String topic = topicComboBox.getSelectionModel().getSelectedItem();
             Edition edition = table.getSelectionModel().getSelectedItem();
             User author = controller.getUserById(loggedUser.getId());
-            for(Paper p: controller.getAllPapers() ) {
-                if(p.getTitle().equals(title) && p.getAuthor().getUsername().equals(loggedUser.getUsername()) && p.getEdition().getId() == edition.getId())
-                {
+            for (Paper p : controller.getAllPapers()) {
+                if (p.getTitle().equals(title) && p.getAuthor().getUsername().equals(loggedUser.getUsername()) && p.getEdition().getId() == edition.getId()) {
                     //System.out.println("intra la update paper");
-                    Paper paper = new Paper(author, edition, PaperStatus.InReview, title, topic, pdfData);
+                    Paper paper = new Paper(author, edition, PaperStatus.InReview, title, topic, pdfData, pdfDataSummary);
                     p.setTopic(topic);
                     p.setPaperPDF(pdfData);
+                    p.setSummaryPDF(pdfDataSummary);
                     controller.updatePaper(p);
                     ShowAlert.showOnSucces("Paper Succesfully updated");
                     return;
                 }
             }
 
-            Paper paper = new Paper(author, edition, PaperStatus.InReview, title, topic, pdfData);
+            Paper paper = new Paper(author, edition, PaperStatus.InReview, title, topic, pdfData, pdfDataSummary);
             controller.addPaper(paper);
             ShowAlert.showOnSucces("Paper Succesfully added");
-        }
-        catch (ServiceException exception)
-        {
+        } catch (ServiceException exception) {
             ShowAlert.showAlert("Paper could not be added");
         }
 
     }
-
 
 
     @Override
